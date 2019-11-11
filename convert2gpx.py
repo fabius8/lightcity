0c4ff87b0f943632f88e997fd2015bd8330f1ad5
fabius8 <fabius8@163.com>
2019-11-11 18:17:25 +0800

    --wip--

--- convert2gpx.py
+++ convert2gpx.py
@@ -2,6 +2,15 @@
 
 import json
 from urllib import request, parse
+import argparse
+
+parser = argparse.ArgumentParser("convert2gpx.py city.json")
+parser.add_argument("cityjson", help="convert json to gpx.", type=str)
+args = parser.parse_args()
+cityjson = args.cityjson
+citygpx = cityjson.replace(".json", ".gpx")
+print(cityjson, citygpx)
+
 
 class Geocoding:
     def __init__(self, api_key):
@@ -41,16 +50,17 @@ if __name__ == '__main__':
     head += "  version=\"1.1\"\n"
     head += "  creator=\"fabius8@163.com\"\n>"
     g = Geocoding('f8ca14edcad37856646fadd5d84bf512')  # 这里填写你的高德api的key
-    info = json.load(open("city.json"))
+    info = json.load(open(cityjson))
 
-    with open('output.gpx', 'w') as the_file:
+    with open(citygpx, 'w') as the_file:
         the_file.write(head)
 
     count = 0
     for i in info["city"]:
         count += 1
         result = g.geocode(i)
-        with open('output.gpx', 'a') as the_file:
+        print(i, result)
+        with open(citygpx, 'a') as the_file:
             latlon = '\n  <wpt '
             latlon += 'lat="' + str(result[1]) + '" '
             latlon += 'lon="' + str(result[0]) + '">\n'
@@ -58,13 +68,13 @@ if __name__ == '__main__':
             latlon += '    <time>' + "2019-01-01T00:" + str(count).zfill(2) + ":00Z</time>\n"
             latlon += '  </wpt>\n'
             latlon += '  <wpt '
-            latlon += 'lat="' + str(result[1] + 0.001) + '" '
-            latlon += 'lon="' + str(result[0] + 0.001) + '">\n'
+            latlon += 'lat="' + str(result[1] + 0.005) + '" '
+            latlon += 'lon="' + str(result[0] + 0.005) + '">\n'
             latlon += '    <name>' + i + '</name>\n'
             latlon += '    <time>' + "2019-01-01T00:" + str(count).zfill(2) + ":59Z</time>\n"
             latlon += '  </wpt>\n'
             the_file.write(latlon)
 
-    with open('output.gpx', 'a') as the_file:
+    with open(citygpx, 'a') as the_file:
         the_file.write('</gpx>')
 
