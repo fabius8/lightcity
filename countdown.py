dc13fbc8c327844b16593c8758d7d054dceb2da6
fabius8 <fabius8@163.com>
2019-11-30 21:17:59 +0800

    --wip--

--- countdown.py
+++ countdown.py
@@ -1,6 +1,14 @@
 #!/usr/local/bin/python3
 
 import time
+import datetime
+import os
+
+now = datetime.datetime.now()
+seconds = now.hour * 60 * 60 + now.minute * 60 + now.second
+last_seconds = 86400 - seconds
+print("Last seconds to 00:00 left", last_seconds, "secs")
+
 
 def countdown(t):
     while t:
@@ -11,5 +19,9 @@ def countdown(t):
         t -= 1
     print('Blast Off!!!')
 
+
 t = input("Enter the time in seconds: ")
+if t == "":
+    t = last_seconds
 countdown(int(t))
+os.system("say blast off")
