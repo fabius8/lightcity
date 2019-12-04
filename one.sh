336139c66ecfe6ff19f7a155b7b5285e90760a28
fabius8 <fabius8@163.com>
2019-12-04 17:44:36 +0800

    --wip--

--- one.sh
+++ one.sh
@@ -5,9 +5,15 @@ cd tools/
 
 cd ..
 
-for file in tools/1*.json
-do
-    lightcity.py $file
+echo "Do you wish to run lightcity?" 
+
+file=`ls tools/1*.json`
+
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) ./lightcity.py $file; break;;
+        No ) exit;;
+    esac
 done
 
 mv tools/1*.json .
