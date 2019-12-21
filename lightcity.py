37e3885ef78a9ecc03da9c8a9445eee168e86ee8
fabius8 <fabius8@163.com>
2019-12-21 20:23:41 +0800

    --wip--

--- lightcity.py
+++ lightcity.py
@@ -71,9 +71,9 @@ def amap_login(c, s):
     s(type='TextField').set_text(username+'\n')
     time.sleep(5)
     s(type='SecureTextField').set_text(passwd+'\n')
-    # time.sleep(1)
+    time.sleep(1)
     s(name=u'登录').tap()
-    # time.sleep(2)
+    time.sleep(2)
     s(name=u'返回').tap()
     time.sleep(5)
     login_image = username + "-" + time.strftime("%Y%m%d-%H%M%S") + "-login" + ".png"
